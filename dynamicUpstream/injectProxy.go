188edc97392f0ce2e729f7880933cc70b425bec3
roc king <lastboy1228@gmail.com>
2022-12-02 07:55:59 +0000

    升级prom版本

--- dynamicUpstream/injectProxy.go
+++ dynamicUpstream/injectProxy.go
@@ -6,9 +6,10 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"reflect"
 	"strings"
 
-	"github.com/prometheus/prometheus/pkg/labels"
+	"github.com/prometheus/prometheus/model/labels"
 	"github.com/prometheus/prometheus/promql/parser"
 )
 
@@ -19,8 +20,8 @@ var (
 )
 
 const (
-	queryParam    = "query"
-	matchersParam = "match[]"
+	queryParam = "query"
+	matchParam = "match[]"
 )
 
 func init() {
@@ -44,6 +45,7 @@ type Routes struct {
 
 type route struct {
 	labelMatchers map[string]*labels.Matcher
+	request       *http.Request
 }
 
 func NewRoutes() *Routes {
@@ -53,31 +55,78 @@ func NewRoutes() *Routes {
 	}
 	mux.Handle("/api/v1/query", customUpstream())
 	mux.Handle("/api/v1/query_range", customUpstream())
+	mux.Handle("/api/v1/query_exemplars", customUpstream())
+	mux.Handle("/api/v1/series", customUpstream())
 	mux.Handle("/", defaultProxy)
 	return routes
 }
 
 func customUpstream() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		values := req.URL.Query()
-		exp, err := parser.ParseExpr(values.Get(queryParam))
-		if err != nil {
+		log.Printf("[%v]%v", req.RemoteAddr, req.RequestURI)
+		params := req.URL.Query()
+		var useQuery bool = false
+		var useMatch bool = false
+		var expressions []string = params[matchParam]
+		var query string = params.Get(queryParam)
+		if len(query) > 0 {
+			useQuery = true
+		} else if len(expressions) > 0 {
+			useMatch = true
+		}
+		if !useQuery && !useMatch {
+			defaultProxy.ServeHTTP(w, req)
 			return
 		}
-		r := route{labelMatchers: make(map[string]*labels.Matcher)}
+		if useQuery {
+			expressions = append(expressions, query)
+		}
+
+		r := &route{labelMatchers: make(map[string]*labels.Matcher), request: req}
 		////////////////////////////////////////////////////////
-		// get labels concerned
+		// 用于判断路由的关键label
 		r.labelMatchers["hostip"] = nil
-		err = r.parseNode(exp)
-		if err != nil {
-			http.Error(w, "parsing query exp failed", http.StatusInternalServerError)
-			return
+		r.labelMatchers["job"] = nil
+		r.labelMatchers["exporter"] = nil
+		r.labelMatchers["zone"] = nil
+		var matchParamCount int = 0
+		for _, exp := range expressions {
+			if len(exp) == 0 {
+				continue
+			}
+
+			expParsed, err := parser.ParseExpr(exp)
+			if err != nil {
+				log.Printf("error exp %v : %v", exp, err)
+				return
+			}
+			// parser.Call实例：irate(ClickHouseProfileEvents_RealTimeMicroseconds{cluster="wzp", zone="china"}[150s])
+			// parser.MatrixSelector实例：ClickHouseProfileEvents_RealTimeMicroseconds{cluster="wzp", zone="us"}[150s]
+			// parser.VectorSelector实例：ClickHouseProfileEvents_RealTimeMicroseconds{cluster="wzp", zone="jp"}
+			log.Printf("原表达式%s，类型%v", exp, reflect.TypeOf(expParsed))
+			r.parseNode(expParsed)
+			paramValue := expParsed.String()
+			log.Printf("新表达式%s", paramValue)
+			// 重新设置request的参数
+			if useQuery {
+				params.Set(queryParam, paramValue)
+			} else {
+				if matchParamCount == 0 {
+					params.Set(matchParam, paramValue)
+				} else {
+					// array参数，从第二个开始使用Add进行添加
+					params.Add(matchParam, paramValue)
+				}
+				matchParamCount++
+			}
 		}
-		r.getProxy().ServeHTTP(w, req)
+		proxy := r.getProxy()
+		req.URL.RawQuery = params.Encode()
+		proxy.ServeHTTP(w, req)
 	})
 }
 
-////////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////
 // customer this func to dynamic proxy to upstream
 func (r *route) getProxy() *httputil.ReverseProxy {
 	label := r.labelMatchers["hostip"]
@@ -93,8 +142,10 @@ func (r *route) getProxy() *httputil.ReverseProxy {
 	return defaultProxy
 }
 
+// 解析label匹配条件，并移除对zone的匹配
 func (r *route) parseNode(node parser.Node) error {
 	switch n := node.(type) {
+	// 把获取复合查询内部的MatrixSelector、VectorSelector
 	case *parser.EvalStmt:
 		if err := r.parseNode(n.Expr); err != nil {
 			return err
@@ -145,14 +196,12 @@ func (r *route) parseNode(node parser.Node) error {
 	// nothing to do
 
 	case *parser.MatrixSelector:
-		log.Printf("%v", n)
 		if vs, ok := n.VectorSelector.(*parser.VectorSelector); ok {
-			r.matchLabels(vs.LabelMatchers)
+			r.matchLabels(vs)
 		}
 
 	case *parser.VectorSelector:
-		log.Printf("%v", n)
-		r.matchLabels(n.LabelMatchers)
+		r.matchLabels(n)
 
 	default:
 		panic(fmt.Errorf("parser.Walk: unhandled node type %T", n))
@@ -161,11 +210,19 @@ func (r *route) parseNode(node parser.Node) error {
 	return nil
 }
 
-func (r *route) matchLabels(sources []*labels.Matcher) {
-	for _, source := range sources {
+func (r *route) matchLabels(v *parser.VectorSelector) {
+	index := -1
+	for i, source := range v.LabelMatchers {
+		if strings.Compare(source.Name, "zone") == 0 {
+			index = i
+		}
 		if _, ok := r.labelMatchers[source.Name]; ok {
 			r.labelMatchers[source.Name] = source
 		}
+	}
+	if index >= 0 {
+		// 移除zone的匹配条件
+		v.LabelMatchers = append(v.LabelMatchers[:index], v.LabelMatchers[index+1:]...)
 	}
 }
 
--- go.mod
+++ go.mod
@@ -1,15 +1,7 @@
 module github.com/lastboy1228/prom-reverse-proxy
 
-go 1.13
-
-require (
-	github.com/efficientgo/tools/core v0.0.0-20210201224146-3d78f4d30648
-	github.com/go-openapi/runtime v0.19.28
-	github.com/go-openapi/strfmt v0.20.1
-	github.com/go-zookeeper/zk v1.0.2
-	github.com/pkg/errors v0.9.1
-	github.com/prometheus/alertmanager v0.22.2
-	github.com/prometheus/prometheus v1.8.2-0.20210621150501-ff58416a0b02
-)
+go 1.16
 
 replace github.com/prometheus/alertmanager => github.com/prometheus/alertmanager v0.22.1-0.20210623090652-e3fb99cc2d24
+
+require github.com/prometheus/prometheus v2.33.5+incompatible
